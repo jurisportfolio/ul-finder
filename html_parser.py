a71ea9ee92b414def3163649d7e4a0b233cc8dba
jurko <jurko.bossetroll@gmail.com>
2024-08-05 10:41:50 +0200

    Move restored_li func into html_parser

--- html_parser.py
+++ html_parser.py
@@ -72,3 +72,19 @@ class UlExtractor(HTMLParser):
 def html_string(url: str) -> str:
     bite_html = request.urlopen(url).read()
     return bite_html.decode("utf8")
+
+
+def restored_li(raw_li: dict) -> str:
+    li = ""
+    for item in raw_li:
+        if 'start' in item:
+            li += f"<{item['start']}"
+            if 'attrs' in item:
+                for attr in item['attrs']:
+                    li += f" {attr[0]}=\"{attr[1]}\""
+            li += ">"
+        if 'data' in item:
+            li += f"{item['data']}"
+        if 'end' in item:
+            li += f"</{item['end']}>"
+    return li
--- ul_finder.py
+++ ul_finder.py
@@ -3,23 +3,6 @@
 import html_parser
 import cli_args_parser
 
-
-def restored_li(raw_li: dict) -> str:
-    li = ""
-    for item in raw_li:
-        if 'start' in item:
-            li += f"<{item['start']}"
-            if 'attrs' in item:
-                for attr in item['attrs']:
-                    li += f" {attr[0]}=\"{attr[1]}\""
-            li += ">"
-        if 'data' in item:
-            li += f"{item['data']}"
-        if 'end' in item:
-            li += f"</{item['end']}>"
-    return li
-
-
 html_url = cli_args_parser.source_url()
 
 html = html_parser.html_string(html_url)
@@ -30,6 +13,8 @@ extractor.feed(html)
 
 greatest_ul = extractor.greatest_ul()
 
+last_li = html_parser.restored_li(greatest_ul["last_li"])
+
 print(greatest_ul)
 
-print(restored_li(greatest_ul["last_li"]))
+print(last_li)
