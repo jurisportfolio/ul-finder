4ca0c6cd4e55b7594beed2739d76d2f642655335
jurko <jurko.bossetroll@gmail.com>
2024-08-11 10:20:18 +0200

    Add Ul extractor and greatest_ul function

--- ul_finder/data_containers.py
+++ ul_finder/data_containers.py
@@ -14,15 +14,3 @@ class Stack:
 
     def pop(self):
         return self.stack.pop(-1)
-
-
-class Storage:
-    def __init__(self):
-        self.storage = []
-
-    def push(self, last_ul):
-        self.storage.append(last_ul)
-
-    def greatest_ul(self):
-        all_uls = sorted(self.storage, key=operator.itemgetter('count'))
-        return all_uls[-1]
--- ul_finder/html_parser.py
+++ ul_finder/html_parser.py
@@ -1,76 +1,99 @@
 from html.parser import HTMLParser
-from urllib import request
 from ul_finder import data_containers
 
 
 class UlExtractor(HTMLParser):
-    def __init__(self, storage):
+    def __init__(self):
         super().__init__()
         self.stack = data_containers.Stack()
-        self.storage = storage
+        self.storage = []
+        self.ul_counter = 0
+        self.li_counter = 0
+
+    def upraise_ul_counter(self):
+        self.ul_counter += 1
+
+    def upraise_li_counter(self):
+        self.li_counter += 1
+
+    def reset_li_counter(self):
+        self.li_counter = 0
 
     def handle_starttag(self, tag, attrs):
         if tag == 'ul':
-            self.stack.push({"count": 0})
+            self.upraise_ul_counter()
+            self.reset_li_counter()
+            self.stack.push({"ul": self.ul_counter, "li": self.li_counter})
         elif tag == 'li':
+            self.upraise_li_counter()
             last_ul_ref = self.stack.last()
-            last_ul_ref["count"] += 1
-            last_ul_ref["last_li"] = [{"start": tag, "attrs": attrs}]
-        else:
-            last_ul_ref = self.stack.last()
-            if last_ul_ref:
-                if "last_li" in last_ul_ref:
-                    last_ul_ref["last_li"].append({"start": tag, "attrs": attrs})
+            last_ul_ref["li"] = self.li_counter
 
     def handle_endtag(self, tag):
         if tag == "ul":
             last_ul_ref = self.stack.pop()
-            self.storage.push(last_ul_ref)
-        elif tag == "li":
-            last_ul_ref = self.stack.last()
-            last_ul_ref["last_li"].append({"end": tag})
-        else:
-            last_ul_ref = self.stack.last()
-            if last_ul_ref:
-                if "last_li" in last_ul_ref:
-                    last_ul_ref["last_li"].append({"end": tag})
+            self.storage.append(last_ul_ref)
 
-    def handle_data(self, data):
-        # if not data.startswith("\n"):
-        last_ul_ref = self.stack.last()
-        if last_ul_ref:
-            if "last_li" in last_ul_ref:
-                last_ul_ref["last_li"].append({"data": data})
+    def __all_uls__(self):
+        return self.storage
 
-    def handle_startendtag(self, tag, attrs):
-        last_ul_ref = self.stack.last()
-        if last_ul_ref:
-            if "last_li" in last_ul_ref:
-                last_ul_ref["last_li"].append({"self": tag, "attrs": attrs})
+    def greatest_ul(self) -> dict:
+        greatest_ul = self.storage[0]
+        for ul in self.storage:
+            if ul["li"] > greatest_ul["li"]:
+                greatest_ul = ul
+        return greatest_ul
 
 
-def html_string(url: str) -> str:
-    html_bytes = request.urlopen(url).read()
-    return html_bytes.decode("utf8")
+# class LiExtractor(HTMLParser):
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.ul_counter = 0
+#         self.li_counter = 0
+#
+#     def upraise_ul_counter(self):
+#         self.ul_counter += 1
+#
+#     def upraise_li_counter(self):
+#         self.li_counter += 1
+#
+#     def handle_starttag(self, tag, attrs):
+#
+#     def handle_data(self, data):
+#         # if not data.startswith("\n"):
+#         last_ul_ref = self.stack.last()
+#         if last_ul_ref:
+#             if "last_li" in last_ul_ref:
+#                 last_ul_ref["last_li"].append({"data": data})
+#
+#     def handle_startendtag(self, tag, attrs):
+#         last_ul_ref = self.stack.last()
+#         if last_ul_ref:
+#             if "last_li" in last_ul_ref:
+#                 last_ul_ref["last_li"].append({"self": tag, "attrs": attrs})
+#
+#
 
-
-def restored_li(raw_li: dict) -> str:
-    li = ""
-    for item in raw_li:
-        if 'start' in item:
-            li += f"<{item['start']}"
-            if 'attrs' in item:
-                for attr in item['attrs']:
-                    li += f" {attr[0]}=\"{attr[1]}\""
-            li += ">"
-        if 'self' in item:
-            li += f"<{item['self']}"
-            if 'attrs' in item:
-                for attr in item['attrs']:
-                    li += f" {attr[0]}=\"{attr[1]}\""
-            li += " />"
-        if 'data' in item:
-            li += f"{item['data']}"
-        if 'end' in item:
-            li += f"</{item['end']}>"
-    return li
+#
+#
+# def restored_li(raw_li: dict) -> str:
+#     li = ""
+#     for item in raw_li:
+#         if 'start' in item:
+#             li += f"<{item['start']}"
+#             if 'attrs' in item:
+#                 for attr in item['attrs']:
+#                     li += f" {attr[0]}=\"{attr[1]}\""
+#             li += ">"
+#         if 'self' in item:
+#             li += f"<{item['self']}"
+#             if 'attrs' in item:
+#                 for attr in item['attrs']:
+#                     li += f" {attr[0]}=\"{attr[1]}\""
+#             li += " />"
+#         if 'data' in item:
+#             li += f"{item['data']}"
+#         if 'end' in item:
+#             li += f"</{item['end']}>"
+#     return li
