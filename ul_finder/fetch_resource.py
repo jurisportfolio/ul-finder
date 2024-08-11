1f2da4619754c10bde760cabdda8375dfb58942f
jurko <jurko.bossetroll@gmail.com>
2024-08-11 10:03:29 +0200

    Add html fetching error handling

--- ul_finder/fetch_resource.py
+++ ul_finder/fetch_resource.py
@@ -2,8 +2,15 @@ from urllib import request, error
 
 
 def html(url: str) -> str:
-    response = request.urlopen(url)
-    charset = response.headers.get_content_charset()
-    if not charset or type(charset) != str:
-        charset = "utf8"
-    return response.decode(charset)
+    try:
+        response = request.urlopen(url)
+        charset = response.headers.get_content_charset()
+        if charset is not str:
+            charset = "utf8"
+        return response.read().decode(charset)
+
+    except error.HTTPError as e:
+        print(f"HTTP Error: {e.code} {e.reason}")
+
+    except error.URLError as e:
+        print(f"URL Error: {e.reason}")
