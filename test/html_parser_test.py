d6aba89d4fa51e47ff7a39f1e37c84e30b110e1b
jurko <jurko.bossetroll@gmail.com>
2024-08-12 11:49:11 +0200

    Add LastLi tests

--- test/html_parser_test.py
+++ test/html_parser_test.py
@@ -1,5 +1,5 @@
 import unittest
-from ul_finder_src.html_parser import UlExtractor
+from ul_finder_src.html_parser import UlExtractor, LiExtractor
 
 
 class UlExtractorTest(unittest.TestCase):
@@ -19,3 +19,27 @@ class UlExtractorTest(unittest.TestCase):
         self.assertEqual(extractor.ul_counter, 1)
         self.assertEqual(extractor.li_counter, 0)
         self.assertEqual(extractor.storage[-1], {"ul": 1, "has_li": 1})
+
+
+class LiExtractorTests(unittest.TestCase):
+    def test_1(self):
+        position = {"ul": 1, "has_li": 1}
+        extractor = LiExtractor()
+        extractor.provide_ul(position)
+        extractor.handle_starttag("li", None)
+        extractor.handle_starttag("ul", None)
+        extractor.handle_starttag("li", None)
+        extractor.handle_starttag("p", None)
+        extractor.handle_startendtag("img", [("alt", "some img")])
+        extractor.handle_endtag("p")
+        extractor.handle_endtag("li")
+        extractor.handle_endtag("ul")
+        self.assertEqual(extractor.last_li, "<li><p><img alt=\"some img\"/></p></li>")
+
+    def test_2(self):
+        position = {"ul": 1, "has_li": 2}
+        extractor = LiExtractor()
+        extractor.provide_ul(position)
+        extractor.handle_starttag("ul", None)
+        extractor.handle_starttag("li", None)
+
